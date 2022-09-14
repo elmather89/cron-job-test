fcf136a2bebe29dd803e5fcc780f47d9071d6441
lizmather <liz@gpsimpact.com>
2022-09-14 18:25:27 -0500

    .

--- src/cron-ping.js
+++ src/cron-ping.js
@@ -2,7 +2,13 @@ import cron from 'node-cron';
 
 let count = 0;
 
-cron.schedule('* 10 * * * * ', function() {
+// cron.schedule('* * * * * * ', function() {
+//     count++;
+//     console.log(`count: ${count}, date: ${new Date()}`);
+// });
+
+function timer() {
     count++;
-    console.log(`count: ${count}, date: ${new Date()}`);
-});
+    console.log(count);
+};
+timer();
